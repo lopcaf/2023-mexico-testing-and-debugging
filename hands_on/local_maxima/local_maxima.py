d79cd844e31d1152e9a1e2f2620efcc5463ac9a7
ASPP Student <student@localhost>
2023-06-27 14:49:26 -0600

    Tests y maxima

--- hands_on/local_maxima/local_maxima.py
+++ hands_on/local_maxima/local_maxima.py
@@ -7,4 +7,35 @@ def local_maxima(x):
     Output:
     idx -- list of indices of the local maxima in x
     """
-    return []
+    localmax = []
+    for e in x:
+        if e.index == 0:
+            if e > e.index+1:
+                localmax.append(e)
+            else:
+                continue
+
+        elif e.index == len(x):
+            if e > e.index-1:
+                localmax.append(e)
+            else:
+                continue
+            
+        elif e > x.index(e)+1 and e > x.index(e)-1:
+            localmax.append(e)
+        
+        else:
+            continue            
+    return localmax
+
+
+t1 = [1, 3 ,-2, 0, 2, 1]
+t2 = [-1,-1,0,-1]
+t3 = [4,2,1,3,1,5]
+t4 = [1,2,2,1]
+t5 = [1,2,2,3,1]
+test = [t1, t2, t3, t4, t5]
+for e in test:
+    a,b = local_maxima(e)
+    print(f"Result {e}")
+    print(a,b)
